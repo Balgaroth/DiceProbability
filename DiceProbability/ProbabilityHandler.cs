c8df42285e6dc1f82beec22db34eaa5513e7e831
StianF <stian.friberg@banenor.no>
2020-07-21 15:02:46 +0200

    first

---
+++ DiceProbability/ProbabilityHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DiceProbability
+{
+    public class ProbabilityHandler
+    {
+        public double GetProbability(int numberOfDice, int numberOfSuccesses)
+        {
+            List<List<int>> combinationList = new List<List<int>>();
+
+            PopulateCombinationList(numberOfDice, 1, combinationList);
+
+            return CalculateProbaility(combinationList, numberOfSuccesses);
+        }
+
+        private double CalculateProbaility(List<List<int>> combinationList, int numberOfSuccesses)
+        {
+            int success = combinationList.Count(c => c.Count(d => d == 6) >= numberOfSuccesses);
+
+            return (double) success / combinationList.Count;
+        }
+
+        private void PopulateCombinationList(int numberOfDice, int dice, List<List<int>> combinationList)
+        {
+            while (true)
+            {
+                if (dice == 1)
+                {
+                    for (int i = 1; i <= 6; i++) combinationList.Add(new List<int> {i});
+                }
+                else
+                {
+                    List<List<int>> tmpCombinationList = new List<List<int>>();
+
+                    foreach (var combination in combinationList)
+                    {
+                        for (int i = 2; i <= 6; i++)
+                        {
+                            var list = new List<int>(combination);
+                            list.Add(i);
+                            tmpCombinationList.Add(list);
+                        }
+
+                        combination.Add(1);
+                    }
+
+                    combinationList.AddRange(tmpCombinationList.Select(x => x).ToList());
+                }
+
+                if (dice < numberOfDice)
+                {
+                    dice++;
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
---
+++ DiceProbability/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DiceProbability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(args[0], out var numberOfDice);
+            int.TryParse(args[1], out var numberOfSuccesses);
+
+            var handler = new ProbabilityHandler();
+            var probability = handler.GetProbability(numberOfDice, numberOfSuccesses);
+
+            Console.WriteLine($"{probability*100:G3}%");
+        }
+    }
+}
